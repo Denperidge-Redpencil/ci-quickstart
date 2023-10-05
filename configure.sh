b005cca2767fab16edd5d5df301b844bc4c48b59
Denperidge <denperidge@gmail.com>
2023-10-05 16:45:49 +0200

    Renamed .gh/README, updated configure, gh workflow
    - Apparently ./.github/README.md takes priority over ./README.md, wild!
    - Configure now creates the .woodpecker folder,
      and moves the extracted files into it
    - Configure now exits out of select after finishing its first run
    - The GitHub workflow will now include configure.sh
    - Configure now uses a /tmp directory
    - Updated .gitignore

--- configure.sh
+++ configure.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 
-select extract in emberjs python
+select type in emberjs python
 do
-    curl -O https://github.com/Denperidge-Redpencil/ci-quickstart/releases/latest/download/$extract.tar.gz
-    tar -xzf $extract.tar.gz
+    mkdir -p /tmp/ci-quickstart/
+    tar=/tmp/ci-quickstart/$type.tar.gz
+    
+    curl -L -o $tar https://github.com/Denperidge-Redpencil/ci-quickstart/releases/latest/download/$type.tar.gz
+    tar -xzf $tar -C /tmp/ci-quickstart/
 
-    for file in $(find $extract -name "*.yml")
+    for file in $(find /tmp/ci-quickstart -name "*.yml")
     do
-        read -p "Enable ${file#$extract/}? [Y/n] " enable
+        read -p "Enable ${file#$type/}? [Y/n] " enable
         case $enable in
             [Nn]*) rm $file ;;
         esac
     done
+
+    rm $tar
+
+    mkdir -p .woodpecker
+    mv /tmp/ci-quickstart/*/* .woodpecker/
+
+    rmdir /tmp/ci-quickstart/*
+    rmdir /tmp/ci-quickstart/
+    
+    break
 done
